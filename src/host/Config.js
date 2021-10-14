ae0d66a93dcba257ef7eb53cbaed48945ee98d7e
jamshid-zayniyev <84299973+jamshid-zayniyev@users.noreply.github.com>
2021-10-14 03:05:52 -0700

    asdasd

--- src/host/Config.js
+++ src/host/Config.js
@@ -34,18 +34,33 @@ export const editTeachers = (teacher, id) => {
   return httpRequest(config);
 };
 
-export const getXodimlar = () => {
+export const getXodim = () => {
   var config = {
     url: `${url}/xodim/`,
     method: "get",
   };
   return httpRequest(config);
 };
-
+export const createXodim = (dataS) => {
+  var config = {
+    url: `${url}/xodim/`,
+    method: "post",
+    data: dataS,
+  };
+  return httpRequest(config);
+};
 export const deleteXodim = (id) => {
   var config = {
     url: `${url}/xodim/${id}/`,
     method: "delete",
+  };
+  return httpRequest(config);
+};
+export const editXodim = (dataS, id) => {
+  var config = {
+    url: `${url}/xodim/${id}/`,
+    method: "patch",
+    data: dataS,
   };
   return httpRequest(config);
 };
